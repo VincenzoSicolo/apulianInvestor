7bc903b9e8e94450e557ff84d7c1a0ca4da8cdf9
Vincenzo Sicolo <vincenzosicolo@outlook.it>
2023-01-13 19:42:10 +0100

    project setup with Vue3 Inertia and Laravel -1

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Index</div>
+    <Link href="/hello">Show Page</Link>
+    <div>
+        the message {{ message }}
+    </div>
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/inertia-vue3'
+
+defineProps({
+    message: String,
+})
+</script>
+
