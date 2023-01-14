36e4023e7ac9d0e4da78febac7a67e1a8c0f5a46
Vincenzo Sicolo <vincenzosicolo@outlook.it>
2023-01-14 13:06:07 +0100

    form validation request in controller prevent err

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,16 +1,30 @@
 <template>
     <Link href="/hello">Show Page</Link>&nbsp;
     <Link href="/">Main Page</Link>
-    <div>timer {{ timer }}</div>
+    <!-- <div>timer {{ timer }}</div> -->
+    <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+  <div>{{ y }}</div>
     <slot>Default</slot>
 </template>
 
 
 <script setup>
-import {Link} from '@inertiajs/inertia-vue3'
-import {ref} from 'vue'
-
-const timer = ref(0)
-setInterval(() => timer.value++,  1000)
+import { computed } from 'vue'
+import { Link, usePage } from '@inertiajs/inertia-vue3'
+// const x = ref(0)
+// const y = computed(() => x.value * 2)
+const page = usePage()
+const flashSuccess = computed(
+  () => page.props.value.flash.success,
+)
+// import { ref } from 'vue'
+// const timer = ref(0)
+// setInterval(() => timer.value++, 1000)
 </script>
-
+<style>
+.success{
+    color: red;
+}
+</style>
