ec6abcecdc8cf5f303c55fe2f1ab0bb8b21a5a70
Vincenzo Sicolo <vincenzosicolo@outlook.it>
2023-01-14 11:38:36 +0100

    Create, Components, Controller added. form submit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
-use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 
 
 Route::get('/', [IndexController::class, 'index']);
 
 Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class)
+  ->only(['index', 'show', 'create', 'store']);
+
+//   Route::get('listing/index', [ListingController::class], 'index');
+//   Route::get('listing/show', [ListingController::class], 'show');
+
+//   Route::post('listing/create', [ListingController::class], 'create');
