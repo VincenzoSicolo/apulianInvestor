e66132f267cf767d9782a40dbc6f318c4e2e8a18
Vincenzo Sicolo <vincenzosicolo@outlook.it>
2023-01-15 20:30:42 +0100

    listing show style, box ui implemented

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,7 +1,6 @@
 <template>
-    <Link href="/listing/create">New Listing</Link>
 
-    <header class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-700 bg-white">
+    <header class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-900 bg-white">
       <div class="container mx-auto">
         <nav class="p-4 flex items-center justify-between">
           <div class="text-lg font medium">
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,8 +1,11 @@
 <template>
-    <div v-for="listing in listings" :key="listing.id">
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 ">
+      <Box v-for="listing in listings" :key="listing.id" class="p-3">
       <div>
         <Link :href="route('listing.show', {listing: listing.id})">
-        <ListingAddress :listing="listing" />
+          <Price :price="listing.price" class="text-2xl font-bold"/>
+          <ListingSpace :listing="listing" class="text-lg" />
+        <ListingAddress :listing="listing" class="text-gray-500" />
       </Link>
       </div>
       <div>
@@ -11,12 +14,16 @@
       <div>
         <Link :href="route('listing.destroy', {listing: listing.id})" method="delete" as="button">Delete</Link>
       </div>
+    </Box>
     </div>
   </template>
   
   <script setup>
   import {Link} from '@inertiajs/inertia-vue3'
   import ListingAddress from '@/Components/ListingAddress.vue'
+  import Box from '@/Components/UI/Box.vue';
+  import Price from '@/Components/Price.vue';
+import ListingSpace from '@/Components/ListingSpace.vue';
   defineProps({
     listings: Array,
   })
